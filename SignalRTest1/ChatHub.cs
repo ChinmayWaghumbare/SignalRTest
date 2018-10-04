37b580e2653defa2e5455abcbaae799d63883420
Chinmay <cwaghumbar@gmail.com>
2018-10-04 16:11:11 +0530

    Complete Project

--- SignalRTest1/ChatHub.cs
+++ SignalRTest1/ChatHub.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using Microsoft.AspNet.SignalR;
+
+//namespace SignalRTest1
+//{
+//    public class ChatHub : Hub
+//    {
+//        public void Hello()
+//        {D:\Chinmay\SignalRTest1\SignalRTest1\index.html
+//            Clients.All.hello();
+//        }
+//    }
+//}
+
+
+using System;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-
 namespace SignalRTest1
 {
     public class ChatHub : Hub
     {
-        public void Hello()
+        public void Send(string name, string message)
         {
-            Clients.All.hello();
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
         }
     }
 }
